fec548027256ae02a4242c5fcd63e6e47d01d098
davi <davifernadesr@gmail.com>
2026-03-15 15:19:41 -0300

    alterando a matricula do funcionario para 4 digitos

--- internal/model/funcionario.go
+++ internal/model/funcionario.go
@@ -2,20 +2,20 @@ package model
 
 type FuncionarioINserir struct {
 	Nome            string `json:"nome" binding:"required,min=3,max=150"`
-	Matricula       string `json:"matricula" binding:"required,min=7,max=7"`
-	ID_departamento int   `json:"id_departamento" binding:"required,min=1"`
-	ID_funcao       int   `json:"id_funcao"  binding:"required,min=1"`
+	Matricula       string `json:"matricula" binding:"required,min=4,max=4"`
+	ID_departamento int    `json:"id_departamento" binding:"required,min=1"`
+	ID_funcao       int    `json:"id_funcao"  binding:"required,min=1"`
 }
 
 type Funcionario_Dto struct {
-	ID           int             `json:"id"`
-	Nome         string          `json:"nome"`
-	Matricula    string             `json:"matricula"`
-	Funcao       FuncaoDto       `json:"funcao"`
+	ID        int       `json:"id"`
+	Nome      string    `json:"nome"`
+	Matricula string    `json:"matricula"`
+	Funcao    FuncaoDto `json:"funcao"`
 }
 
 type UpdateFuncionarioRequest struct {
-    Nome           *string `json:"nome"`            // Ponteiro! Se for nil, não atualiza
-    IdDepartamento *int    `json:"id_departamento"` // Ponteiro!
-    IdFuncao       *int    `json:"id_funcao"`       // Ponteiro!
+	Nome           *string `json:"nome"`            // Ponteiro! Se for nil, não atualiza
+	IdDepartamento *int    `json:"id_departamento"` // Ponteiro!
+	IdFuncao       *int    `json:"id_funcao"`       // Ponteiro!
 }
