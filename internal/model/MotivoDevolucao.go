e99a3c7d5923b3b4997ef2ddc0abaf059b3a0c84
davi <davifenadesr@gmail.com>
2026-05-01 23:51:30 -0300

    remove espaços em branco desnecessários no modelo de motivo de devolução

--- internal/model/MotivoDevolucao.go
+++ internal/model/MotivoDevolucao.go
@@ -1,12 +1,10 @@
 package model
 
-
 type MotivoDevolucao struct {
 	Motivo string `json:"motivo" binding:"required"`
 }
 
 type MotivoDevolucaoEpiDto struct {
-	Id     int             `json:"id"`
+	Id     int    `json:"id"`
 	Motivo string `json:"motivo"`
 }
-
