6f13b97171b553c4101721f1839dd60fb5482792
davi <davifenadesr@gmail.com>
2026-06-08 14:38:14 -0300

    atualiza logs de erro para melhor rastreamento no LoginController e TenantId

--- controller/LoginController.go
+++ controller/LoginController.go
@@ -111,7 +111,7 @@ func (l *LoginController) Login() gin.HandlerFunc {
 		token, user, err := l.service.FazerLogin(c, input.Email, input.Senha, tenantID)
 		if err != nil {
 
-			log.Println("erro ao realizar login: %w", err)
+			log.Printf("erro ao realizar login: %v", err)
 			if err.Error() == "email ou senha inválidos" {
 
 				c.JSON(http.StatusUnauthorized, gin.H{
